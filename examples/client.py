3b28179b98e82433d2e6a4dba78f3494ecf94eed
Arina <arina.bubenina@mail.ru>
2025-12-03 18:07:26 +0700

    Update client.py

--- examples/client.py
+++ examples/client.py
@@ -1,18 +1,14 @@
 import socket
 import time
 
-# Создаем сокет
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Подключаемся к серверу
 client_socket.connect(('127.0.0.1', 12346))
 
-# Отправляем данные серверу
 for i in range(5):
     client_socket.sendall(b'Hello, server!')
     response = client_socket.recv(1024)
     print(f"Получен ответ от сервера: {response}")
     time.sleep(1)
 
-# Закрываем соединение
 client_socket.close()
