30be74d9694beb5d48db61c8f72d541bf6254fae
Arina <arina.bubenina@mail.ru>
2026-03-09 12:16:40 +0700

    добавлен корректный вывод данных на экран

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@
 #include "backends/imgui_impl_opengl3.h"
 #include "backends/imgui_impl_sdl2.h"
 #include <mutex>
+#include <regex>
 
 using namespace std;
 
@@ -17,10 +18,13 @@ struct location {
     double latitude;
     double longitude;
     double altitude;
+    double accuracy;          
     double current_time;
     string cell_info;
+    long long traffic_rx;       
+    long long traffic_tx;      
     
-    location() : latitude(0), longitude(0), altitude(0), current_time(0), cell_info("") {}
+    location() : latitude(0), longitude(0), altitude(0), accuracy(0), current_time(0), cell_info(""), traffic_rx(0), traffic_tx(0) {}
 };
 
 void thread1(location *loc, mutex *mtx){
@@ -29,12 +33,12 @@ void thread1(location *loc, mutex *mtx){
         cout << "Ошибка открытия файла";
         return;
     }
-    try{
-    zmq::context_t context(1);
-    zmq::socket_t socket (context, ZMQ_REP);
-    socket.bind("tcp://0.0.0.0:5556");
-    int count = 0;
-    cout << "Сервер запущен и ожидает подключений"<< endl;
+    try {
+        zmq::context_t context(1);
+        zmq::socket_t socket (context, ZMQ_REP);
+        socket.bind("tcp://0.0.0.0:5559");
+        int count = 0;
+        cout << "Сервер запущен и ожидает подключений"<< endl;
 
     while (true){
         zmq::message_t request;
@@ -44,49 +48,58 @@ void thread1(location *loc, mutex *mtx){
                 continue;
             }
         std::string data(static_cast<char*>(request.data()), request.size());
-        cout << "!!! ПОЛУЧЕНО: " << data << endl;  
         
         count++;
- 
-        size_t delimiter_pos = data.find(" | ");
-        if (delimiter_pos != string::npos) {
-            string coords = data.substr(0, delimiter_pos);
-            string cell_info = data.substr(delimiter_pos + 3);
+
+        size_t first_delim = data.find(" | ");
+        size_t second_delim = data.find(" | RX=");
+        
+        if (first_delim != string::npos && second_delim != string::npos) {
+            string coords = data.substr(0, first_delim);
+            string cell_info = data.substr(first_delim + 3, second_delim - (first_delim + 3));
+            string traffic_str = data.substr(second_delim + 6); 
             
             cout << "Координаты: " << coords << endl;
-            
-            double lon, lat, alt;
+            cout << "Cell Info: " << cell_info << endl;
+            cout << "Трафик: " << traffic_str << endl;
+            double lon, lat, alt, acc;
             long long tim; 
-            
-            if (sscanf(coords.c_str(), "%lf,%lf,%lf,%lld", &lon, &lat, &alt, &tim) == 4) {
+            long long rx = 0, tx = 0;
+     
+            if (sscanf(coords.c_str(), "%lf,%lf,%lf,%lf,%lld", &lon, &lat, &alt, &acc, &tim) == 5) {
+            bool traffic_parsed = false;
+
+            if (sscanf(traffic_str.c_str(), "%lld TX=%lld", &rx, &tx) == 2) {
+                cout << "Парсинг: rx=" << rx << " tx=" << tx << endl;
+                traffic_parsed = true;
+            }
+
+            if (traffic_parsed) {
                 lock_guard<mutex> gui(*mtx);
                 loc->longitude = lon;
                 loc->latitude = lat;
                 loc->altitude = alt;
+                loc->accuracy = acc;                 
                 loc->current_time = tim;
                 loc->cell_info = cell_info;
+                loc->traffic_rx = rx;                
+                loc->traffic_tx = tx;               
                 
-                cout << "! lon=" << lon << ", lat=" << lat 
-                     << ", alt=" << alt << ", time=" << tim << endl;
-                
-                F << "Longitude: " << loc->longitude 
-                  << ", Latitude: " << loc->latitude 
-                  << ", Altitude: " << loc->altitude 
-                  << ", Time: " << loc->current_time 
-                  << ", Cell Info: " << loc->cell_info  << endl;
+                F << "Longitude: " << loc->longitude << ", Latitude: " << loc->latitude << ", Altitude: " << loc->altitude<< ", Accuracy: " << loc->accuracy << ", Time: " 
+                << loc->current_time << ", Cell Info: " << loc->cell_info << ", Traffic RX: " << loc->traffic_rx << ", Traffic TX: " << loc->traffic_tx << endl;
                 F.flush();
-            } else {
-                cout << "ОШИБКА парсинга координат!" << endl;
-            }
-        } else {
-            cout << "Нет разделителя ' | ' в данных!" << endl;
+                } else {
+            cout << "Ошибка парсинга трафика: " << traffic_str << endl;
         }
+    } else {
+        cout << "Ошибка парсинга координат: " << coords << endl;
+    }
+}
         
         zmq::message_t reply(3);
         memcpy(reply.data(), "OK", 3);
         socket.send(reply, zmq::send_flags::none);
-
-        cout << "кол-во полученных данных: " << count << endl;
+        cout << "Кол-во полученных данных: " << count << endl;
     }
     
 }
@@ -94,14 +107,76 @@ void thread1(location *loc, mutex *mtx){
         cerr << "ZeroMQ ошибка: " << e.what() << endl;
     }
     catch(const exception& e) {
-        cerr << "ошибка сервера: " << e.what() << endl;
+        cerr << "Ошибка сервера: " << e.what() << endl;
     }
     F.close();
 }
+void showField(const string& text, const regex& r, const char* label, const char* unit = "") {
+    smatch match;
+    if (regex_search(text, match, r)) {
+        if (strlen(unit) > 0)
+            ImGui::Text("    %s: %s %s", label, match[1].str().c_str(), unit);
+        else
+        ImGui::Text("    %s: %s", label, match[1].str().c_str());
+    }
+}
+void parseCellInfo(const string& cell) {
 
+    if (cell.find("LTE:") != string::npos || cell.find("CellInfoLte") != string::npos) {
+
+        ImGui::TextColored(ImVec4(0,1,0,1), "Network Type: LTE (4G)");
+
+        ImGui::Text("  Cell Identity:");
+
+        showField(cell, regex("mMcc=([0-9]+)"), "MCC");
+        showField(cell, regex("mMnc=([0-9]+)"), "MNC");
+        showField(cell, regex("mCi=([0-9]+)"), "Cell ID");
+        showField(cell, regex("mPci=([0-9]+)"), "PCI");
+        showField(cell, regex("mTac=([0-9]+)"), "TAC");
+        showField(cell, regex("mEarfcn=([0-9]+)"), "EARFCN");
+        showField(cell, regex("mBandwidth=([0-9]+)"), "Bandwidth");
+
+        ImGui::Text("  Signal Strength:");
+
+        showField(cell, regex("rsrp=(-?[0-9]+)"), "RSRP", "dBm");
+        showField(cell, regex("rsrq=(-?[0-9]+)"), "RSRQ", "dB");
+        showField(cell, regex("rssi=(-?[0-9]+)"), "RSSI", "dBm");
+        showField(cell, regex("rssnr=([0-9]+)"), "RSSNR");
+        showField(cell, regex("cqi=([0-9]+)"), "CQI");
+        showField(cell, regex("ta=([0-9]+)"), "Timing Advance");
+    }
+
+    else if (cell.find("GSM:") != string::npos || cell.find("CellInfoGsm") != string::npos) {
+
+        ImGui::TextColored(ImVec4(1,1,0,1), "Network Type: GSM (2G/3G)");
+
+        ImGui::Text("  Cell Identity:");
+
+        showField(cell, regex("mMcc=([0-9]+)"), "MCC");
+        showField(cell, regex("mMnc=([0-9]+)"), "MNC");
+        showField(cell, regex("mArfcn=([0-9]+)"), "ARFCN");
+        showField(cell, regex("mBsic=([0-9]+)"), "BSIC");
+        showField(cell, regex("mLac=([0-9]+)"), "LAC");
+        showField(cell, regex("rssi=([0-9]+)"), "RSSI");
+    }
+
+    else if (cell.find("NR:") != string::npos || cell.find("CellInfoNr") != string::npos) {
+
+        ImGui::TextColored(ImVec4(1,0,0,1), "Network Type: NR (5G)");
+
+        showField(cell, regex("mNci=([0-9]+)"), "NCI");
+        showField(cell, regex("mPci=([0-9]+)"), "PCI");
+        showField(cell, regex("mTac=([0-9]+)"), "TAC");
+        showField(cell, regex("mNrargcn=([0-9]+)"), "NRARFCN");
+
+        showField(cell, regex("SS-RSRP=(-?[0-9]+)"), "SS-RSRP", "dBm");
+        showField(cell, regex("SS-RSRQ=(-?[0-9]+)"), "SS-RSRQ", "dB");
+        showField(cell, regex("SS-SINR=(-?[0-9]+)"), "SS-SINR", "dB");
+    }
+}
 void run_gui(location *loc, mutex *mtx){
     SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window* window = SDL_CreateWindow("Location and Cell Info", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 700, SDL_WINDOW_OPENGL);
+    SDL_Window* window = SDL_CreateWindow("Location and Cell Info", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 900, 800, SDL_WINDOW_OPENGL);
     SDL_GLContext gl_context = SDL_GL_CreateContext(window);
 
     ImGui::CreateContext();
@@ -122,22 +197,32 @@ void run_gui(location *loc, mutex *mtx){
         
         ImGui::Begin("Location and Cell Info");
         lock_guard<mutex> lock(*mtx);
-        ImGui::Text("Latitude: %.6f", loc->latitude);
+    
+        ImGui::TextColored(ImVec4(1,1,0,1), "Location");
+        ImGui::Text("Latitude:  %.6f", loc->latitude);
         ImGui::Text("Longitude: %.6f", loc->longitude);
-        ImGui::Text("Altitude: %.2f", loc->altitude);
-        ImGui::Text("Time: %.0f", loc->current_time);
+        ImGui::Text("Altitude:  %.2f m", loc->altitude);
+        ImGui::Text("Accuracy:  %.2f m", loc->accuracy); 
+        ImGui::Text("Time:      %.0f ms", loc->current_time);
+        
         ImGui::Separator();
-        ImGui::Text("information about the cell");
+  
+        ImGui::TextColored(ImVec4(0,1,1,1), "CellInfo");
         string cell = loc->cell_info;
-        //тут переделать
-        if (cell.find("LTE:") != string::npos || cell.find("CellInfoLte") != string::npos) {
-            ImGui::TextColored(ImVec4(0,1,0,1), "network type: LTE (4G)");
-        } else if (cell.find("GSM:") != string::npos || cell.find("CellInfoGsm") != string::npos) {
-            ImGui::TextColored(ImVec4(1,1,0,1), "network type: GSM (2G)");
-        } else if (cell.find("NR:") != string::npos || cell.find("CellInfoNr") != string::npos) {
-            ImGui::TextColored(ImVec4(1,0,0,1), "network type: NR (5G)");
-        }
-        ImGui::TextWrapped("%s", cell.c_str());
+       
+        parseCellInfo(cell);
+      
+        ImGui::Separator();
+        
+        ImGui::TextColored(ImVec4(1,0.5,0,1), "Network traffic");
+ 
+        double rx_mb = loc->traffic_rx / (1024.0 * 1024.0);
+        double tx_mb = loc->traffic_tx / (1024.0 * 1024.0);
+        double total_mb = (loc->traffic_rx + loc->traffic_tx) / (1024.0 * 1024.0);
+        
+        ImGui::Text("Received (RX): %lld bytes (%.2f MB)", loc->traffic_rx, rx_mb);
+        ImGui::Text("Sent (TX):     %lld bytes (%.2f MB)", loc->traffic_tx, tx_mb);
+        ImGui::TextColored(ImVec4(0,1,0,1), "Total:          %.2f MB", total_mb);
         
         ImGui::End();
 
